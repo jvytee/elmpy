d77a344034ca9eec90e8f5575b00ee9d0bb06c64
Julian Theis <julian.theis@posteo.de>
2020-02-22 19:48:26 +0100

    Add basic unit tests

--- tests/test_elm.py
+++ tests/test_elm.py
@@ -1,4 +1,6 @@
 import pytest
+import numpy as np
+
 from elmpy import ELMRegressor
 
 
@@ -6,6 +8,31 @@ class TestELMRegressor:
     def test_init(self):
         elm = ELMRegressor()
 
+        assert elm.n_hidden == 50
+        assert elm.alpha == 1.0
+        assert elm.gamma is None
+
         assert elm.w_in is None
         assert elm.w_out is None
         assert elm.bias is None
+
+    def test_fit(self):
+        elm = self.fit_elm()
+
+        assert elm.w_in.shape == (50, 1)
+        assert elm.bias.shape == (50,)
+        assert elm.w_out.shape == (50, 2)
+
+    def test_predict(self):
+        elm = self.fit_elm()
+
+        assert elm.predict(np.array([5])).shape == (2,)
+
+    def fit_elm(self):
+        xs = np.vstack(np.arange(0, 10, 0.1))
+        ys = np.hstack([np.sin(xs), np.cosh(xs)])
+
+        elm = ELMRegressor()
+        elm.fit(xs, ys)
+
+        return elm
